bc185dd9157185256cd94e80291366921e41f99a
Lily Bennu <lilyrunsthematrix@gmail.com>
2023-10-05 00:10:41 -0500

    post tests completed

--- server/src/test/java/app/cartoons/data/PostJdbcTemplateRepositoryTest.java
+++ server/src/test/java/app/cartoons/data/PostJdbcTemplateRepositoryTest.java
@@ -1,33 +1,80 @@
 package app.cartoons.data;
 
+import app.cartoons.models.User;
+import app.cartoons.models.Post;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class PostJdbcTemplateRepositoryTest {
 
+    static final int MISSING_ID = 99;
+
+    @Autowired
+    PostJdbcTemplateRepository  repository;
+    @Autowired
+    KnownGoodState knownGoodState;
+
     @BeforeEach
-    void setUp() {
+    void setup() {
+        knownGoodState.set();
     }
 
     @Test
     void findAll() {
+        List<Post> posts = repository.findAll();
+        assertNotNull(posts);
     }
 
     @Test
     void findById() {
+        Post testPost = repository.findById(1);
+        assertEquals(1, testPost.getPostId());
+        assertEquals("Hello World!", testPost.getTitle());
     }
 
     @Test
     void add() {
+        Post post = new Post();
+        post.setTitle("Potato");
+        post.setCaption("Aberdeen in da fields");
+        post.setReference("Amazing World of Gumball");
+        post.setUserId(1);
+        post.setPostId(3);
+
+        boolean add = repository.add(post);
+
+        assertTrue(add);
     }
 
     @Test
     void update() {
+        Post post = new Post();
+        post.setTitle("Potato");
+        post.setCaption("Aberdeen in da fields");
+        post.setReference("Amazing World of Gumball");
+        post.setUserId(1);
+        post.setPostId(3);
+
+        Post existingPost = repository.findById(3);
+
+        if (existingPost != null) {
+            existingPost.setTitle("Idaho");
+            post.setCaption("In da fields, POTATO!");
+            post.setReference("Amazing World of Gumball");
+
+            boolean updated = repository.update(existingPost);
+            assertEquals("Idaho", existingPost.getTitle());
+        }
     }
 
     @Test
     void deleteById() {
+        assertTrue(repository.deleteById(1));
     }
 }
