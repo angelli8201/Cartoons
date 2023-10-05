acbeae492feef09155e51541ce64d031d55fd005
Lily Bennu <lilyrunsthematrix@gmail.com>
2023-10-05 14:10:23 -0500

    mwhahhahah all data tests pass now, fixed some minor issues in post repository and mapper

--- server/src/main/java/app/cartoons/data/mappers/PostMapper.java
+++ server/src/main/java/app/cartoons/data/mappers/PostMapper.java
@@ -10,10 +10,10 @@ public class PostMapper implements RowMapper<Post>{
     @Override
     public Post mapRow(ResultSet resultSet, int i) throws SQLException {
         Post post = new Post();
-        post.setPostId(resultSet.getInt("user_id"));
-        post.setTitle(resultSet.getString("user_name"));
-        post.setCaption(resultSet.getString("pass_word"));
-        post.setReference(resultSet.getString("`reference`"));
+        post.setPostId(resultSet.getInt("post_id"));
+        post.setTitle(resultSet.getString("title"));
+        post.setCaption(resultSet.getString("caption"));
+        post.setReference(resultSet.getString("reference"));
         post.setUserId(resultSet.getInt("user_id"));
 
         return post;
